3a0906fce81667f6d534c0d05d30ee3add69fcd1
Tim Lai <tlai4@disroot.org>
2020-10-09 15:30:00 -0400

    Restored accidentally deleted code

--- seurat_preprocessing.R
+++ seurat_preprocessing.R
@@ -37,6 +37,108 @@ seurat_obj <- map(seurat_obj, function(x) {
   return(x)
 })
 
+## Prepare plots
+
+mt_data <- map(seurat_obj, function(x) {
+  meta_data <- x@meta.data
+  meta_data <- as_tibble(meta_data, .name_repair = "unique")
+  meta_data$cells <- rownames(meta_data) 
+  meta_data <- meta_data %>% rename(nUmi = nCount_RNA, nGene = nFeature_RNA)
+  return(meta_data)
+})
+
+mt_data$DMSO$sample <- "DMSO"
+mt_data$EZH2$sample <- "EZH2"
+mt_data$RACi$sample <- "RACi"
+mt_data$Combo$sample <- "Combo"
+
+mt_data <- bind_rows(mt_data)
+
+p <- ggplot(mt_data, aes(x = percent.mt, y = nGene)) +
+  geom_point(size = 0.1) +
+  facet_wrap(. ~ orig.ident, ncol = 3, scales = "free")
+
+if (!dir.exists(file.path("results", "preprocessing"))) {
+  dir.create(file.path("results", "preprocessing"))
+}
+
+pdf(file.path("results", "preprocessing", "mt_content.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(x=sample, fill=sample)) + 
+  geom_bar() + theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  theme(plot.title = element_text(hjust=0.5, face="bold")) +
+  ggtitle("NCells")
+
+pdf(file.path("results", "preprocessing", "num_cells.pdf"))
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(color=sample, x=nUmi, fill=sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  ylab("Cell density") +
+  geom_vline(xintercept = 500)
+pdf(file.path("results", "preprocessing", "umi_counts.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(color=sample, x = nGene, fill= sample))  +
+  geom_density(alpha = 0.2) +
+  theme_classic() +
+  scale_x_log10() +
+  geom_vline(xintercept = 300)
+pdf(file.path("results", "preprocessing", "genes_per_cell.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+
+p <- ggplot(mt_data, aes(x = sample, y = log10(nGene), fill=sample))+
+  geom_boxplot() +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  theme(plot.title = element_text(hjust=0.5, face="bold")) +
+  ggtitle("NCells vs NGenes")
+
+pdf(file.path("results", "preprocessing", "nGenes.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(x = nUmi, y = nGene, color = percent.mt)) +
+  geom_point() +
+  scale_colour_gradient(low = "gray90", high = "black") +
+  stat_smooth(method=lm) +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_classic() +
+  geom_vline(xintercept = 500) +
+  geom_hline(yintercept = 250) +
+  facet_wrap(~sample)
+pdf(file.path("results", "preprocessing", "umi_v_genes.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(color=sample, x = percent.mt, fill=sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  geom_vline(xintercept = 20)
+pdf(file.path("results", "preprocessing", "mt_density.pdf"), height = 10, width = 10)
+p
+dev.off()
+
+p <- ggplot(mt_data, aes(x=log10GenesPerUMI, color = sample, fill=sample)) +
+  geom_density(alpha = 0.2) +
+  theme_classic() +
+  geom_vline(xintercept = 0.8)
+
+pdf(file.path("results", "preprocessing", "complexity.pdf"), height = 10, width = 10)
+p
+dev.off()
+
 seurat_obj <- map(seurat_obj, function(x) {
   x <- subset(x, subset = percent.mt <= 20
               & nFeature_RNA > 1000 & log10GenesPerUMI > 0.8)
@@ -124,32 +226,3 @@ dev.off()
 ## ----------
 
 saveRDS(seurat_integrated, file.path("results", "r_objects", "seurat_integrated.RDS"))
-
-## Marker Analysis
-
-if (!dir.exists(file.path("results", "markers"))) {
-  dir.create(file.path("results", "markers"))
-}
-
-get_conserved <- function(cluster){
-  FindConservedMarkers(seurat_integrated,
-                       ident.1 = cluster,
-                       grouping.var = "orig.ident",
-                       only.pos = TRUE, 
-                       assay = "SCT", 
-                       slot = "data") %>%
-    rownames_to_column(var = "gene") %>%
-    left_join(y = unique(annotations[, c("gene_name", "description")]),
-              by = c("gene" = "gene_name")) %>%
-    cbind(cluster_id = cluster, .)
-}
-annotations <- read.csv("annotation.csv")
-
-conserved_markers <- map_dfr(1:20, get_conserved)
-
-top10 <- conserved_markers %>% 
-  mutate(avg_fc = (control_avg_logFC + crispr_avg_logFC) /2) %>% 
-  group_by(cluster_id) %>% 
-  top_n(n = 10, 
-        wt = avg_fc)
-write.table(top10, file.path("results", "markers", "top10.csv"), sep=",", col.names=TRUE, row.names=FALSE, quote=FALSE)
